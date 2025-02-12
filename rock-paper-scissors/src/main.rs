4fad5acb213dcd29f95c85a2e6578c3a8af93be5
seungdols <seungdols0822@gmail.com>
2025-02-12 14:26:35 +0900

    feat: Add section2

---
+++ rock-paper-scissors/src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    println!("[가위, 바위, 보] 중 하나를 입력해주세요!\n");
+
+    let mut decision = String::new();
+
+
+    io::stdin().read_line(&mut decision).expect("입력 실패");
+
+    println!("당신의 선택은 {}", decision);
+}
