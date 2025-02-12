63cf20b2502d7c14fa333c2d2da87f3f9a2217fb
seungdols <seungdols0822@gmail.com>
2025-02-12 17:50:08 +0900

    feat: 기본 데이터 타입

--- section3/src/main.rs
+++ section3/src/main.rs
@@ -21,5 +21,19 @@ fn main() {
         println!("안쪽 범위에서 x의 값은 {x} 입니다");
     }
     println!("x의 값은 {x} 입니다");
+
+
+    let add = 3 + 8;
+    let sub = 26.5 - 2.1;
+    let mul = 7 * 20;
+    let quotient = 12.0 / 3.14;
+    let truncated  = 7 / 5; // 결과는 1
+    let remainder = 46 % 5;
+
+    let t = true;
+    let f: bool = false;
+
+    let c = 'A';
+    let z: char = '가';
 }
 
