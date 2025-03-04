62bba3a2f07b13ba54f3293a22debcdc97063d5c
seungdols <seungdols0822@gmail.com>
2025-03-04 15:19:18 +0900

    feat: Enum과 패턴 매칭

--- section6/src/main.rs
+++ section6/src/main.rs
@@ -16,6 +16,37 @@ enum Message {
 //     None,
 //     Some(T)
 // }
+
+struct RGB(u8, u8, u8);
+fn color_to_rgb(color: Color) -> RGB {
+    match color {
+        Color::Red => RGB(255, 0, 0),
+        Color::Green => RGB(0, 255, 0),
+        Color::Blue => RGB(0, 0, 255),
+    }
+}
+
+fn handle_message(message: &Message) {
+    match message {
+        Message::StartGame =>
+            println!("게임을 시작합니다."),
+
+        Message::WinPoint { who } =>
+            println!("{}의 득점", who),
+
+        Message::ChangePlayerName(name) => println!("플레이어의 이름 변경 => {}", name),
+
+        _ => println!("아직 구현 하지 않은 메시지"),
+    }
+}
+
+fn increment(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main() {
 
     let red = Color::Red;
@@ -44,4 +75,7 @@ fn main() {
     println!("absent_number = {:?}", absent_number);
 
     // some_number + 1; // Error
+    let x = Some(2);
+    println!("{:?}", increment(x));
+    println!("{:?}", increment(None));
 }
