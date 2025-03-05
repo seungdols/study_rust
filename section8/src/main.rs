033b09194a584c33378834444af731442cd07235
seungdols <seungdols0822@gmail.com>
2025-03-05 23:54:26 +0900

    feat: trait 제네릭 타입의 범위를 한정하기

--- section8/src/main.rs
+++ section8/src/main.rs
@@ -45,6 +45,67 @@ impl<T> Point<T> {
     }
 }
 
+trait Greet {
+    fn greeting(&self) -> String;
+}
+
+#[derive(Debug)]
+enum Pet {
+    Dog,
+    Cat,
+    Tiger,
+}
+
+impl Greet for Pet {
+    fn greeting(&self) -> String {
+        match self {
+            Pet::Dog => String::from("멍멍"),
+            Pet::Cat => String::from("야옹"),
+            Pet::Tiger => String::from("어흥"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    active: bool,
+}
+
+impl Greet for Person {
+    fn greeting(&self) -> String {
+        String::from("안녕하세요")
+    }
+}
+
+// fn meet(one: &impl Greet, another: &impl Greet) {
+//     println!("첫번째가 인사합니다: {}", one.greeting());
+//     println!("두번째가 인사합니다: {}", another.greeting()));
+// }
+
+use std::fmt::Debug;
+use std::fmt::Display;
+
+// fn meet<T: Greet + Debug, U: Greet + Display>(one: &T, another: &U) {
+//     println!("첫번째가 인사합니다: {}", one.greeting());
+//     println!("두번째가 인사합니다: {}", another.greeting());
+// }
+
+fn meet<T, U>(one: &T, another: &U)
+where T: Greet + Debug,
+      U: Greet + Display
+{
+    println!("첫번째가 인사합니다: {}", one.greeting());
+    println!("두번째가 인사합니다: {}", another.greeting());
+}
+
+impl Display for Person {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // write!(f, "{}", self.name)
+        f.write_str(&self.name.as_str())
+    }
+}
+
 fn main() {
     let numbers = vec![3,4,1,6,8,10];
     let result = smallest(&numbers);
@@ -60,4 +121,11 @@ fn main() {
     let p1 = Point { x: 2, y:3 };
     let p2 = Point { x: 5.0, y: 1.0};
     println!("p1 = {:?}, p2 = {:?}", p1, p2);
+
+    let cat = Pet::Cat;
+    let gildong = Person {
+        name: String::from("홍길동"),
+        active: true,
+    };
+    meet(&cat, &gildong);
 }
